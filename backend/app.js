4536c11e0fc0d0912ed9fa1de0a65d29a1680192
Alexis Fuentes <lexisnvm@gmail.com>
2024-03-08 07:43:12 -0700

    primer

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+require ('dotenv').config( );
+const express = require('express');
+const app = express();
+const port = 3000;
+const postsRoutes = require ('./src/routes/postsRoutes');
+const usersRoutes = require ('./src/routes/usersRoutes')
+const mongoDB = require('./src/db/db');
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/', postsRoutes);
+app.use('/', usersRoutes);
+
+mongoDB.connect.then((message)=>{
+    console.log(message);
+    app.listen(port, ()=>{
+        console.log(`Serving reading in port ${port}`);
+    })
+}).catch((error)=>{
+    console.log(error);
+})
---
+++ frontend/create.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttonJS = document.getElementById('button')
+    
+    buttonJS.addEventListener('click', (e) => {
+        e.preventDefault()
+        let usernameJS = document.getElementById('username').value
+        let passwordJS = document.getElementById('password').value
+        
+        let dataFilter = {email:usernameJS, password:passwordJS};
+
+        console.log(dataFilter)
+
+        if(usernameJS && passwordJS) {
+            fetch("http://localhost:3000/create", {
+                method: "Post",
+                body: JSON.stringify(dataFilter),
+                headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                }
+            })
+                .then((response) => response.json())
+                .then((json) => console.log(json));
+        } else {
+            alert("Debe llenar todos los datos")
+        }
+    })
+})
